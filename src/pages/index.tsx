524353ddb74d90c81fd2d26d9a68a8085676c5c1
Renato Hideki Osaka <renato.osaka@hotmail.com>
2021-01-19 08:38:22 -0300

    fix: responsive

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -13,29 +13,29 @@ export default function Home() {
       </Head>
 
       <main className="flex justify-center items-center h-screen antialiased">
-        <div className="flex flex-col justify-center items-center">
+        <div className="flex flex-col justify-center items-center p-4">
           <img src='https://github.com/renatoosaka.png'  width={150} height={150} alt='Renato Hideki Osaka' className="rounded-full" />
           <h1 className="font-medium text-2xl text-gray-900 dark:text-gray-400 self-center mt-4">Renato Hideki Osaka</h1>
-          <p className="dark:text-gray-400 mt-4 leading-6">Sou dev, invento umas paradas maneiras com Node.js ReactJS e React Native</p>
-          <p className="dark:text-gray-400 leading-6">Ahh estou aprendendo Go, muito massinha!</p>
+          <p className="dark:text-gray-400 mt-4 leading-6 text-center">Sou dev, faço umas paradas maneiras com Node.js ReactJS e React Native</p>
+          <p className="dark:text-gray-400 leading-6 text-center">Ahh estou aprendendo Go, muito massinha!</p>
           <div className="flex flex-row mt-4">
             <Link href="https://github.com/renatoosaka/">
-              <a className="dark:text-gray-400 ml-4 hover:text-gray-200 transition">
+              <a className="dark:text-gray-400 ml-4 dark:hover:text-gray-200 transition">
                 <FaGithub size={20} />
               </a>
             </Link>
             <Link href="https://www.linkedin.com/in/renato-hideki-osaka/">
-              <a className="dark:text-gray-400 ml-4 hover:text-gray-200 transition">
+              <a className="dark:text-gray-400 ml-4 dark:hover:text-gray-200 transition">
                 <FaLinkedin size={20} />
               </a>
             </Link>
             <Link href="https://twitter.com/renato_osaka">
-              <a className="dark:text-gray-400 ml-4 hover:text-gray-200 transition">
+              <a className="dark:text-gray-400 ml-4 dark:hover:text-gray-200 transition">
                 <FaTwitter size={20} />
               </a>
             </Link>
             <Link href="mailto:oi@renatohidekiosaka.com.br">
-              <a className="dark:text-gray-400 ml-4 hover:text-gray-200 transition">
+              <a className="dark:text-gray-400 ml-4 dark:hover:text-gray-200 transition">
                 <MdMail size={20} />
               </a>
             </Link>
