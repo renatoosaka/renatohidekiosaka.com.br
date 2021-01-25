1abd6a7280c0ad64c0a95e3601c825a16582d50e
Renato Hideki Osaka <renato.osaka@hotmail.com>
2021-01-25 15:21:05 -0300

    chore: metatags

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -7,6 +7,13 @@ class MyDocument extends Document {
       <Html lang="pt-BR">
         <Head>
           <meta charSet="UTF-8" />
+          <meta property="og:title" content="Renato Hideki Osaka" />
+          <meta property="og:site_name" content="Renato Hideki Osaka" />
+          <meta property="og:type" content="website" />
+          <meta property="og:image" content="https://github.com/renatoosaka.png" />
+          <meta property="og:image:type" content="image/png" />
+          <meta property="og:image:width" content="800" />
+          <meta property="og:image:height" content="600" />
           <link rel="preconnect" href="https://fonts.gstatic.com" />
           <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;700&display=swap" rel="stylesheet" /> 
         </Head>
